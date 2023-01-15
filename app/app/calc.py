ebe3dd06fa84c0cc66dec351baa4a49deba42a03
ggpark <ggpark@ggparkui-MacBookPro.local>
2023-01-15 15:03:50 +0900

    added DB configure

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result"""
+    return x + y
+
+def subtract(x, y):
+    max_x = max(x, y)
+    min_y = min(x, y)
+    return max_x - min_y
