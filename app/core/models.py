5273145ca43bdc7c39c07a0b10524f772b0f18e2
ggpark <ggpark@ggparkui-MacBookPro.local>
2023-01-17 17:10:55 +0900

    added user email validation

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,8 @@
 Database models
 """
 
+import re
+
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -9,12 +11,30 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have an email address.')
+
+        p = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$') # noqa
+
+        if not p.match(email):
+            raise ValueError('Check your email address format.')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
