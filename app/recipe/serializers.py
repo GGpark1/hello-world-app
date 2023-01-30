eea1f4dec23f8d43b5f7a5e0016bde431c67f900
ggpark <ggpark@ggparkui-MacBookPro.local>
2023-01-30 12:06:12 +0900

    implemented tag app CRUD

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -23,10 +23,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
 
-    def create(self, validated_data):
-        """Create a recipe"""
-        tags = validated_data.pop('tags', [])
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed"""
         auth_user = self.context['request'].user
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(
@@ -35,7 +33,30 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def create(self, validated_data):
+        """Create a recipe"""
+        # 'tags'가 있다면 Pop 없다면 빈 리스트를 저장
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe"""
+        # 'tags'가 있다면 Pop 없다면 tags에 None을 부여
+        tags = validated_data.pop('tags', None)
+        # tag를 업데이트 해야하는지 None 여부로 결정
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+
+        return instance
 
 
 class RecipeDetailSerializer(RecipeSerializer):
